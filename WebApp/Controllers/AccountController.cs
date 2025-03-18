9c3d7d0853f6c794c407e779496786710d9fe1d1
Hakan <legion61614@gmail.com>
2025-03-18 15:07:15 +0300

    login handled

--- WebApp/Controllers/AccountController.cs
+++ WebApp/Controllers/AccountController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using WebApp.Dtos.User;
 using WebApp.Interfaces;
 using WebApp.Models;
@@ -13,11 +14,13 @@ public class AccountController : ControllerBase
 {
     private readonly UserManager<User> _userManager;
     private readonly ITokenService _tokenService;
+    private readonly SignInManager<User> _signInManager;
     
-    public AccountController(UserManager<User> userManager, ITokenService tokenService)
+    public AccountController(UserManager<User> userManager, ITokenService tokenService, SignInManager<User> signInManager)
     {
         _userManager = userManager;
         _tokenService = tokenService;
+        _signInManager = signInManager;
     }
 
     [HttpPost("register")]
@@ -30,7 +33,7 @@ public class AccountController : ControllerBase
 
             var user = new User
             {
-                UserName = registerDto.Email,
+                UserName = registerDto.Username,
                 Email = registerDto.Email,
             };
             
@@ -55,5 +58,29 @@ public class AccountController : ControllerBase
         {
             return StatusCode(500, err);
         }
+    }
+
+    [HttpPost("login")]
+    public async Task<IActionResult> Login([FromBody] LoginDto loginDto)
+    {
+        if (!ModelState.IsValid)
+            return BadRequest(ModelState);
+
+        var user = await _userManager.Users.FirstOrDefaultAsync(user => user.UserName == loginDto.UserName.ToLower());
+
+        if (user == null)
+            return Unauthorized("User not found!");
+        
+        var result = await _signInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
+        
+        if(!result.Succeeded)
+            return Unauthorized("Invalid login attempt!");
+
+        return Ok(new NewUserDto
+        {
+            UserName = user.UserName,
+            Email = user.Email,
+            Token = _tokenService.CreateToken(user)
+        });
     }
 }
